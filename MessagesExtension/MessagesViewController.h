eed4af8812a5c3724bc7bb0a3a53bb9fe1394d2d
bangni <bangni@weixinzhuyi.com>
2016-10-19 15:35:29 +0800

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by 从来 on 16/10/19.
+//  Copyright © 2016年 从来. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
